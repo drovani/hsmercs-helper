4dc123146c7332c03ec4e730fd4fcc5be5029eaf
David Rovani <david@rovani.net>
2022-01-06 11:13:58 -0600

    Remove accidentally created js file.

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var plugin_vue_1 = require("@vitejs/plugin-vue");
-var vite_1 = require("vite");
-exports["default"] = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_vue_1["default"])()]
-});
