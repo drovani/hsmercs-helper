f9fbfb81ba195d30cf269fd38384dd4b2e68b8c3
David Rovani <david@rovani.net>
2022-01-13 08:51:32 -0600

    Add TypeScript support for Vuex store in Vue components;

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,17 @@
 /// <reference types="vite/client" />
 
+import { DefineComponent } from 'vue'
+import { Store } from 'vuex'
+import { State } from './store/state'
+
 declare module '*.vue' {
-    import { DefineComponent } from 'vue'
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
     const component: DefineComponent<{}, {}, any>
     export default component
+}
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $store: Store<State>
+    }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
-import { createStore, useStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import getters from "./getters";
 import mutations from "./mutations";
 import { state, State } from "./state";
+
+export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
     state,
@@ -9,8 +12,6 @@ export const store = createStore<State>({
     mutations,
 })
 
-export function getStore() {
-    return {
-        store: useStore()
-    }
+export function useStore() {
+    return vuexUseStore(key);
 }
