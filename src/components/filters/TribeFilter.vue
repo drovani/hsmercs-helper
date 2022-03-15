045c4c2a1cfadb71f0f65a99b95a056e39bee226
David Rovani <david@rovani.net>
2022-03-15 11:19:12 -0500

    Enhanced Tribe Filter to include factions.

--- src/components/filters/TribeFilter.vue
+++ src/components/filters/TribeFilter.vue
@@ -1,36 +1,62 @@
 <template>
-  <div class="flex md:gap-2">
-    <div
-      v-for="rarity in Rarities"
-      :key="rarity"
-      @click="$emit('toggleRarity', rarity)"
-      @dblclick="$emit('filterRarity', rarity)"
-      class="cursor-pointer"
-    >
-      <img
-        class="max-h-8 inline opacity-50"
-        :class="{
-          'opacity-100': enabledRarities.includes(rarity),
-        }"
-        :src="`/images/${rarity.toLowerCase()}.png`"
-        :alt="rarity"
-      />
+  <div class="flex md:gap-2 rounded text-white bg-gray-600 px-2">
+    <div v-for="(faction, label) in constructedTribes">
+      <label>
+        <input
+          type="checkbox"
+          :checked="isFactionChecked(faction)"
+          @change="$emit('toggleFaction', faction)"
+        />
+        {{ label }}
+      </label>
+      <ul class="ml-4">
+        <li v-for="tribe in faction">
+          <label>
+            <input
+              type="checkbox"
+              :checked="enabledTribes.includes(tribe)"
+              :key="tribe"
+              @change="$emit('toggleTribe', tribe)"
+              class="mr-2"
+            />{{ tribe }}
+          </label>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Rarities, Rarity } from "../../models/constants";
+import { computed } from "vue";
+import {
+AllianceTribes,
+HordeTribes,
+NeutralTribes,
+Tribe
+} from "../../models/constants";
 
-defineProps({
-  enabledRarities: {
-    type: Array as () => Rarity[],
+const props = defineProps({
+  enabledTribes: {
+    type: Array as () => Tribe[],
     default: [],
   },
 });
 
+const constructedTribes = computed(() => {
+  return {
+    "Alliance Tribes": [...AllianceTribes],
+    "Horde Tribes": [...HordeTribes],
+    "Neutral Tribes": [...NeutralTribes],
+  };
+});
+
+const isFactionChecked = computed(() => (faction: string[]) => {
+  return faction.every((f) => props.enabledTribes.includes(f as Tribe));
+});
+
 defineEmits<{
-  (event: "toggleRarity", rarity?: Rarity): void;
-  (event: "filterRarity", rarity?: Rarity): void;
+  (event: "toggleTribe", tribe?: Tribe): void;
+  (event: "filterTribe", tribe?: Tribe): void;
+  (event: "toggleFaction", tribes?: Tribe[]): void;
 }>();
 </script>
